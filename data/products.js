b1b5ac7768663eb17697bb4158366a14e5bd6ad6
chitandasom <247357824+chitandasom@users.noreply.github.com>
2025-12-11 22:39:26 +0900

    Add files via upload

---
+++ data/products.js
@@ -0,0 +1,49 @@
+const products = [
+  {
+    id: 1,
+    category: ["all", "♱"],
+    name: "doll wig comb",
+    price: "₩00000",
+    img: "img/1.png",
+
+    description: "구체관절 인형용 빗입니다. ABS-like resin으로 제작되었으며, 가볍고 단단한 재질입니다.",
+
+    detailImages: [
+      "img/1-1.png",
+      "img/1-2.png",
+      "img/1-3.png"
+    ]
+  },
+
+  {
+    id: 2,
+    category: ["all", "custom"],
+    name: "custom face-up",
+    price: "₩00000",
+    img: "img/2.png",
+
+    description: "인형 페이스업 주문 제작 서비스입니다. 스타일에 맞춘 맞춤 작업을 제공합니다.",
+
+    detailImages: [
+      "img/2-1.png",
+      "img/2-2.png",
+      "img/2-3.png"
+    ]
+  },
+
+  {
+    id: 3,
+    category: ["all", "for-doll"],
+    name: "hat for doll",
+    price: "₩00000",
+    img: "img/3.png",
+
+    description: "구체관절 인형용 모자입니다. 부드러운 재질로 제작되었습니다.",
+
+    detailImages: [
+      "img/3-1.png",
+      "img/3-2.png",
+      "img/3-3.png"
+    ]
+  }
+];
